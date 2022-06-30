a827ab27ebb69b04857b06dde698e51277b2f61a
Erlon Barbosa <erlonecia@msn.com>
2022-06-29 21:13:28 -0300

    Course

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -13,3 +13,5 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 1);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
 
+INSERT INTO tb_course (name, img_Uri, img_Gray_Uri) VALUES ('Bootcamp HTML', 'https://live.staticflickr.com/65535/48749461477_24b4e9543b_b.jpg', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQVLjvbw_MPXVfOamAbW4dNk1lnYerrYY-TfQ&usqp=CAU');
+
