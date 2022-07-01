d7f53937b45177ec333cca2a3015f0b94d31a83a
Erlon Barbosa <erlonecia@msn.com>
2022-07-01 14:30:28 -0300

    Resource

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -17,3 +17,7 @@ INSERT INTO tb_course (name, img_Uri, img_Gray_Uri) VALUES ('Bootcamp HTML', 'ht
 
 INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('1.0', TIMESTAMP WITH TIME ZONE '2020-11-20T03:00:00Z',  TIMESTAMP WITH TIME ZONE '2021-11-20T03:00:00Z', 1);
 INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('2.0', TIMESTAMP WITH TIME ZONE '2020-12-20T03:00:00Z',  TIMESTAMP WITH TIME ZONE '2021-12-20T03:00:00Z', 1);
+
+INSERT INTO tb_resource (title, description, position, img_Uri, type, offer_id) VALUES ('Trilha HTML', 'Trilha principal do curso', 1, 'https://live.staticflickr.com/65535/48749461477_24b4e9543b_b.jpg', 1, 1 );
+INSERT INTO tb_resource (title, description, position, img_Uri, type, offer_id) VALUES ('Forum HTML', 'Tire suas dúvidas', 2, 'https://live.staticflickr.com/65535/48749461477_24b4e9543b_b.jpg', 2, 1 );
+INSERT INTO tb_resource (title, description, position, img_Uri, type, offer_id) VALUES ('Lives ', 'Lives exclusiva para a turma', 3, 'https://live.staticflickr.com/65535/48749461477_24b4e9543b_b.jpg', 0, 1 );
